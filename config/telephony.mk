31cf529d47f8fcd0e2d034f23afbc95b30aa9112
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-11 17:08:02 -0700

    apns-config.xml: updated as of SlimLP 2016-06-07
    - update POST Luxembourg APN
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,8 +1,6 @@
-ifeq ($(TARGET_DEVICE),shamu)
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/benzo/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-endif
 
 # Telephony packages
 PRODUCT_PACKAGES += \
