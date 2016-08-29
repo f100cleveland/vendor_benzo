8c0f2894f7b4c8073927788ab7de81c565cbdc25
mydongistiny <jaysonedson@gmail.com>
2016-08-29 05:11:35 -0700

    vendor: Add angler device Makefile
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,4 @@
+ifeq (benzo_angler,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/benzo_angler.mk
+endif
+
---
+++ products/benzo_angler.mk
@@ -0,0 +1,30 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus6P
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/benzo/config/common.mk)
+$(call inherit-product, vendor/benzo/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := angler
+PRODUCT_NAME := benzo_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90M 3085278 release-keys" \
+    BUILD_FINGERPRINT="google/angler/angler:7.0/NRD90M/3085278:user/release-keys"
+
+# Target device
+export TARGET_DEVICE := angler
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
+
