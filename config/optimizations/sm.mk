d0837f27815837399ca5cd841ce12e5a83c53ca8
mydongistiny <jaysonedson@gmail.com>
2016-10-05 22:00:52 -0700

    vendor: Change kernel strings for Linaro
    RIP in Peace SaberMod. GCC is depreciated in Android for the most part except for the kernel.
    We're gonna use Linaro from now on because I'm tired of maintaining SaberMod and prefer Big L now anyways.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/optimizations/sm.mk
+++ config/optimizations/sm.mk
@@ -17,8 +17,7 @@
 # TARGET_SM_AND can be set before this file to override the default of gcc 4.8 for ROM.
 # This is to avoid hardcoding the gcc versions for the ROM and kernels.
 
- TARGET_SM_AND := $(TARGET_GCC_VERSION)
- TARGET_SM_KERNEL := $(TARGET_GCC_VERSION_KERNEL)
+ TARGET_LINARO_KERNEL := $(TARGET_GCC_VERSION_KERNEL)
 
  # Set GCC colors
  export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
@@ -29,16 +28,11 @@
  HOST_OS := linux
 
  # Path to kernel toolchain
- SM_KERNEL_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-$(TARGET_SM_KERNEL)
- SM_KERNEL := $(shell $(SM_KERNEL_PATH)/bin/aarch64-gcc --version)
-
- SM_KERNEL_NAME := $(filter %sabermod,$(SM_KERNEL))
- SM_KERNEL_DATE := $(filter 20140% 20141% 20150% 20151% 2016%,$(SM_KERNEL))
- SM_KERNEL_STATUS := $(filter (release) (prerelease) (experimental),$(SM_KERNEL))
- SM_KERNEL_VERSION := $(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-$(SM_KERNEL_STATUS)
+ LINARO_KERNEL_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-$(TARGET_LINARO_KERNEL)
+ LINARO_VER := $(shell cat $(LINARO_KERNEL_PATH)/VERSION)
 
   PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sm.kernel=$(SM_KERNEL_VERSION)
+    ro.sm.kernel=$(LINARO_VER)
 
  # Add extra libs for the compilers to use
  export LD_LIBRARY_PATH := $(TARGET_ARCH_LIB_PATH):$(LD_LIBRARY_PATH)
