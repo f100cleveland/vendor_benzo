d78d4a0db3fd9ff35797d456b97fac12a30ef33e
mydongistiny <jaysonedson@gmail.com>
2016-12-10 18:02:08 -0800

    vendor: clean up some stuff
    remove "export" from files as it's not needed anymore
    go back to clang 3.9 for now
    clean up props
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/optimizations/optimizations.mk
+++ config/optimizations/optimizations.mk
@@ -1,29 +1,27 @@
 # System
-export USE_CCACHE := 1
-export BLOCK_BUILD := true
+USE_CCACHE := 1
+BLOCK_BUILD := true
 
 # Target device
-export TARGET_DEVICE := angler
+TARGET_DEVICE := angler
 
 # Toolchain and other
-export TARGET_GCC_VERSION_KERNEL := 6.2
+TARGET_GCC_VERSION_KERNEL := 6.2
 
 # benzoCore
-export KBUILD_BUILD_USER := xanaxdroid
-export KBUILD_BUILD_HOST := benzo
+KBUILD_BUILD_USER := xanaxdroid
+KBUILD_BUILD_HOST := benzo
 
 # Jack server heap size
-export ANDROID_JACK_VM_ARGS += "-Xmx4g"
+ANDROID_JACK_VM_ARGS += "-Xmx4g"
 
 # Force Java8
-export LOCAL_JAVA_LANGUAGE_VERSION := 1.8
+LOCAL_JAVA_LANGUAGE_VERSION := 1.8
 
 # Prop Optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.performance.tuning=1 \
     keyguard.no_require_sim=true \
-    persist.service.lgospd.enable=0 \
-    persist.service.pcsync.enable=0 \
     ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=2500 \
@@ -32,22 +30,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.use_intro_anim=false \
     ro.max.fling_velocity=12000 \
     ro.min.fling_velocity=8000 \
-    ro.ril.enable.amr.wideband=1 \
-    ro.sys.fw.bg_apps_limit=20 \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
-    ro.com.google.networklocation=0 \
-    touch.pressure.scale=2 \
     wifi.supplicant_scan_interval=180 \
     windowsmgr.max_events_per_sec=150
-
-# Battery
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.hw_power_saving=1 \
-    ro.ril.disable.power.collapse=0 \
-    persist.radio.add_power_save=1 \
-    persist.sys.use_dithering=0 \
-    pm.sleep_mode=1 \
-    power_supply.wakeup=enable \
-    power.saving.mode=1
-
